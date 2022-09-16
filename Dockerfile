6e59ca69816b5e7312c61208dc98bc6ee588ca53
Daniel Karzel <daniel@comit.network>
2022-09-16 09:49:42 +0200

    Work in review comments

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM ghcr.io/itchysats/itchysats/taker:0.6.1 as builder
 
 ## TODO: It would be nice if we could just use our container, but we are building from distroless - which is great because it's slim, but we don't have any Linux tools in there...
-## Can use busybox to copy necessary functionality in, but run into trouble when trying to access e.g. `dev/stdin` for the health_check
-## Use bullseye because we need glibc >2.29 available
+## Can use busybox to copy necessary functionality in, but could not fix depednency on `dev/stdin` for the health_check
+## Use bullseye instead of buster because we need glibc >2.29 available, bullseye is stable since 10.09.2022
 FROM debian:bullseye-slim
 COPY --from=builder /usr/bin/binary /bin/itchysats
 
@@ -17,7 +17,6 @@ RUN chmod a+x /usr/local/bin/health-check.sh
 
 VOLUME /data
 
-EXPOSE 8000 9999 10000
+EXPOSE 8000
 
-# TODO: Configure the volume here and in the entrypoint
 ENTRYPOINT ["docker_entrypoint.sh"]
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -5,22 +5,23 @@ set -e
 ITCHYSATS_USER=itchysats
 ITCHYSATS_PASSWORD=$(yq e '.password' /root/start9/config.yaml)
 
-# Properties Page
-echo 'version: 2' > /root/start9/stats.yaml
-echo 'data:' >> /root/start9/stats.yaml
-echo '  ItchySats Username:' >> /root/start9/stats.yaml
-echo '    type: string' >> /root/start9/stats.yaml
-echo '    value: "'"$ITCHYSATS_USER"'"' >> /root/start9/stats.yaml
-echo '    description: The username for ItchySats' >> /root/start9/stats.yaml
-echo '    copyable: true' >> /root/start9/stats.yaml
-echo '    masked: false' >> /root/start9/stats.yaml
-echo '    qr: false' >> /root/start9/stats.yaml
-echo '  ItchySats Password:' >> /root/start9/stats.yaml
-echo '    type: string' >> /root/start9/stats.yaml
-echo '    value: "'"$ITCHYSATS_PASSWORD"'"' >> /root/start9/stats.yaml
-echo '    description: The password for ItchySats.' >> /root/start9/stats.yaml
-echo '    copyable: true' >> /root/start9/stats.yaml
-echo '    masked: true' >> /root/start9/stats.yaml
-echo '    qr: false' >> /root/start9/stats.yaml
+# Properties page for username and password
+echo "version: 2
+data:
+  ItchySats Username:
+    type: string
+    value: \"$ITCHYSATS_USER\"
+    description: The username for ItchySats
+    copyable: true
+    masked: false
+    qr: false
+  ItchySats Password:
+    type: string
+    value: \"$ITCHYSATS_PASSWORD\"
+    description: The password for ItchySats.
+    copyable: true
+    masked: true
+    qr: false
+" >> /root/start9/stats.yaml
 
 exec itchysats --data-dir=/data --http-address=0.0.0.0:8000 --password=$ITCHYSATS_PASSWORD mainnet --electrum=electrs.embassy:50001
