c1fdbb2034c96a4cb240d5ebbb9afdcb24dc5a94
Daniel Karzel <daniel@comit.network>
2022-09-16 10:00:58 +0200

    Configure `tor-address` like other wrappers

--- scripts/services/getConfig.ts
+++ scripts/services/getConfig.ts
@@ -1,15 +1,15 @@
 import { compat, types as T } from "../deps.ts";
 
 export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({
-  // "tor-address": {
-  //   "name": "Tor Address",
-  //   "description": "The Tor address of the network interface",
-  //   "type": "pointer",
-  //   "subtype": "package",
-  //   "package-id": "itchtysats",
-  //   "target": "tor-address",
-  //   "interface": "main",
-  // },
+  "tor-address": {
+    "name": "Tor Address",
+    "description": "The Tor address of the network interface",
+    "type": "pointer",
+    "subtype": "package",
+    "package-id": "itchtysats",
+    "target": "tor-address",
+    "interface": "main",
+  },
   "password": {
     "type": "string" as const,
     "nullable": false,
