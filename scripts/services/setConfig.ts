8c49bedd1fa21abfdb1542dba8e1bf28e12351f9
Daniel Karzel <daniel@comit.network>
2022-09-16 10:36:05 +0200

    fixup! Configure `electrs` as required

--- scripts/services/setConfig.ts
+++ scripts/services/setConfig.ts
@@ -1,18 +1,4 @@
-import { compat, types as T } from "../deps.ts";
-
-// deno-lint-ignore require-await
-export const setConfig: T.ExpectedExports.setConfig = async (
-  effects: T.Effects,
-  newConfig: T.Config,
-) => {
-  const dependsOnElectrs: { [key: string]: string[] } =
-    newConfig?.["enable-electrs"] ? { electrs: ["synced"] } : {};
-  const dependsOnBitcoind: { [key: string]: string[] } = newConfig?.txindex
-    ? { bitcoind: [] }
-    : {};
-
-  return compat.setConfig(effects, newConfig, {
-    ...dependsOnElectrs,
-    ...dependsOnBitcoind,
-  });
-};
+import {
+  compat,
+} from "../deps.ts";
+export const setConfig = compat.setConfig;
